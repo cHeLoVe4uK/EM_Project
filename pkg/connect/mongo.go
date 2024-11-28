23c225ce8450bd589648d15d40dc750e322af2a6
Meraiku <meraikuu@gmail.com>
2024-11-28 12:58:58 +0300

    Fix error handling at connect to mongodb

--- pkg/connect/mongo.go
+++ pkg/connect/mongo.go
@@ -2,7 +2,8 @@ package connect
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"log/slog"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -11,18 +12,23 @@ import (
 func NewMongo(ctx context.Context, mongoDSN string) (*mongo.Client, error) {
 	client, err := mongo.Connect(options.Client().ApplyURI(mongoDSN))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("connect mongo: %w", err)
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("ping mongo: %w", err)
 	}
 
 	go func() {
 		<-ctx.Done()
+
+		slog.Debug("Disconnecting mongo")
+
 		if err := client.Disconnect(ctx); err != nil {
-			log.Fatalf("failed to disconnect mongodb: %v", err)
+			slog.Warn("failed to disconnect mongo", "error", err)
 		}
+
+		slog.Debug("Disconnected from mongo successfully")
 	}()
 
 	return client, nil
