7425d1adc9f9b9aae3080741d75f12916260fb10
PariahProg <pariah.prog@gmail.com>
2024-12-02 13:51:04 +0500

    Add service

--- internal/domain/services/auth.go
+++ internal/domain/services/auth.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
@@ -9,10 +10,17 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GetToken(userID string, username string) (string, error) {
+type Service struct {
+}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetTokens(ctx context.Context, user models.User) (models.Tokens, error) {
 	claims := models.Claims{
-		UserID:   userID,
-		Username: username,
+		UserID:   user.UserID,
+		Username: user.Username,
 		RegisteredClaims: jwt.RegisteredClaims{
 			Issuer:    "Chat App",
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
@@ -20,24 +28,29 @@ func GetToken(userID string, username string) (string, error) {
 		},
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 
-	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	accessTokenString, err := accessToken.SignedString([]byte(os.Getenv("SECRET_KEY")))
 	if err != nil {
-		return "", err
+		return models.Tokens{}, err
 	}
-	return tokenString, nil
+
+	tokens := models.Tokens{
+		AccessToken: accessTokenString,
+	}
+
+	return tokens, nil
 }
 
-func Refresh(claims models.Claims) (string, error) { // временно просто вызывает создание нового access токена, так как никакой логики проверки refresh токена пока нет
-	return GetToken(claims.UserID, claims.Username)
+func (s *Service) Refresh(ctx context.Context, user models.User) (models.Tokens, error) { // временно просто вызывает создание нового access токена, так как никакой логики проверки refresh токена пока нет
+	return s.GetTokens(ctx, user)
 }
 
-func Authenticate(token string) (*models.Claims, error) {
-	claims := &models.Claims{}
+func (s *Service) Authenticate(ctx context.Context, tokens models.Tokens) (models.Claims, error) {
+	claims := models.Claims{}
 	secretKey := os.Getenv("SECRET_KEY")
 
-	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+	_, err := jwt.ParseWithClaims(tokens.AccessToken, &claims, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -45,7 +58,7 @@ func Authenticate(token string) (*models.Claims, error) {
 	})
 
 	if err != nil {
-		return nil, err
+		return models.Claims{}, err
 	}
 	return claims, nil
 }
